6c33b6a06b477cd04ea8a1f6cdfdad8642c63357
Zak <89150707+Zak0749@users.noreply.github.com>
2022-07-17 21:45:58 +0100

    cool stuffs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,38 @@
 use self::prelude::*;
 use bevy::render::render_resource::TextureUsages;
+use bevy_inspector_egui::WorldInspectorPlugin;
+use player::Player;
+
+use wall::WallPlugin;
 
 mod player;
 mod prelude;
+mod wall;
 fn main() {
     App::new()
+        .insert_resource(WindowDescriptor {
+            width: 512.0,
+            height: 512.0,
+            ..Default::default()
+        })
         .add_plugins(DefaultPlugins)
+        .add_plugin(WorldInspectorPlugin::new())
         .add_plugin(LdtkPlugin)
         .add_plugin(player::PlayerPlugin)
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
         .insert_resource(LevelSelection::Index(0))
+        .add_plugin(WallPlugin)
+        .insert_resource(LdtkSettings {
+            level_spawn_behavior: LevelSpawnBehavior::UseWorldTranslation {
+                load_level_neighbors: true,
+            },
+            set_clear_color: SetClearColor::FromLevelBackground,
+            ..Default::default()
+        })
         .add_startup_system(setup)
         .add_system(set_texture_filters_to_nearest)
+        .add_system(camera_fit_inside_current_level)
+        .add_system(display_events)
         .run();
 }
 
@@ -20,7 +41,6 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 
     commands.spawn_bundle(LdtkWorldBundle {
         ldtk_handle: asset_server.load("Untitled.ldtk"),
-        transform: Transform::from_xyz(-232., -496., 0.),
         ..Default::default()
     });
 }
@@ -37,5 +57,75 @@ fn set_texture_filters_to_nearest(
                     | TextureUsages::COPY_DST;
             }
         }
+    }
+}
+
+const ASPECT_RATIO: f32 = 1. / 1.;
+
+pub fn camera_fit_inside_current_level(
+    mut camera_query: Query<
+        (
+            &mut bevy::render::camera::OrthographicProjection,
+            &mut Transform,
+        ),
+        Without<Player>,
+    >,
+    player_query: Query<&Transform, With<Player>>,
+    level_query: Query<
+        (&Transform, &Handle<LdtkLevel>),
+        (Without<OrthographicProjection>, Without<Player>),
+    >,
+    level_selection: Res<LevelSelection>,
+    ldtk_levels: Res<Assets<LdtkLevel>>,
+) {
+    if let Ok(Transform {
+        translation: player_translation,
+        ..
+    }) = player_query.get_single()
+    {
+        let player_translation = *player_translation;
+
+        let (mut orthographic_projection, mut camera_transform) = camera_query.single_mut();
+
+        for (level_transform, level_handle) in level_query.iter() {
+            if let Some(ldtk_level) = ldtk_levels.get(level_handle) {
+                let level = &ldtk_level.level;
+                if level_selection.is_match(&0, level) {
+                    let level_ratio = level.px_wid as f32 / ldtk_level.level.px_hei as f32;
+
+                    orthographic_projection.scaling_mode = bevy::render::camera::ScalingMode::None;
+                    orthographic_projection.bottom = 0.;
+                    orthographic_projection.left = 0.;
+                    if level_ratio > ASPECT_RATIO {
+                        // level is wider than the screen
+                        orthographic_projection.top = (level.px_hei as f32 / 9.).round() * 9.;
+                        orthographic_projection.right = orthographic_projection.top * ASPECT_RATIO;
+                        camera_transform.translation.x = (player_translation.x
+                            - level_transform.translation.x
+                            - orthographic_projection.right / 2.)
+                            .clamp(0., level.px_wid as f32 - orthographic_projection.right);
+                        camera_transform.translation.y = 0.;
+                    } else {
+                        // level is taller than the screen
+                        orthographic_projection.right = (level.px_wid as f32 / 16.).round() * 16.;
+                        orthographic_projection.top = orthographic_projection.right / ASPECT_RATIO;
+                        camera_transform.translation.y = (player_translation.y
+                            - level_transform.translation.y
+                            - orthographic_projection.top / 2.)
+                            .clamp(0., level.px_hei as f32 - orthographic_projection.top);
+                        camera_transform.translation.x = 0.;
+                    }
+
+                    camera_transform.translation.x += level_transform.translation.x;
+                    camera_transform.translation.y += level_transform.translation.y;
+                }
+            }
+        }
+    }
+}
+
+fn display_events(mut collision_events: EventReader<CollisionEvent>) {
+    for collision_event in collision_events.iter() {
+        println!("Received collision event: {:?}", collision_event);
     }
 }
--- src/player.rs
+++ src/player.rs
@@ -1,5 +1,4 @@
 use super::prelude::*;
-use bevy::sprite::collide_aabb::collide;
 
 pub struct PlayerPlugin;
 
@@ -7,78 +6,50 @@ impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugin(InputManagerPlugin::<PlayerAction>::default())
             .add_system(player_movement_system)
-            .register_ldtk_entity::<PlayerBundle>("Player")
-            .register_ldtk_int_cell_for_layer::<CollisionLayer>("Collision", 1);
+            .register_ldtk_entity::<PlayerBundle>("Player");
     }
 }
 
-#[derive(Component, Default)]
-struct Player;
-
-#[derive(Component, Default)]
-struct Name {
-    name: String,
-}
-
-#[derive(Bundle, Default)]
+#[derive(Bundle, Default, LdtkEntity)]
 struct PlayerBundle {
     player: Player,
 
-    collider: Collider,
-
     #[bundle]
+    #[from_entity_instance]
+    collider: PlayerCollision,
+    #[bundle]
+    #[sprite_sheet_bundle]
     sprite_bundle: SpriteSheetBundle,
     #[bundle]
-    input_manager: InputManagerBundle<PlayerAction>,
+    input_manager: PlayerInput,
 }
 
-impl LdtkEntity for PlayerBundle {
-    fn bundle_entity(
-        entity_instance: &EntityInstance,
-        _: &LayerInstance,
-        tileset: Option<&Handle<Image>>,
-        tileset_definition: Option<&TilesetDefinition>,
-        _: &AssetServer,
-        texture_atlases: &mut Assets<TextureAtlas>,
-    ) -> PlayerBundle {
-        let tileset = tileset.expect("No titleset provided");
-        let tile = entity_instance.tile.as_ref().expect("No tile provided");
-        let tileset_definition = tileset_definition.expect("No tileset definition provided");
+#[derive(Component, Default)]
+pub struct Player;
 
-        println!("{:?} {:?}", tile.w, tile.h);
+#[derive(Bundle, Default)]
+struct PlayerCollision {
+    collider: Collider,
+    rigid_body: RigidBody,
+    velocity: Velocity,
+    gravity_scale: GravityScale,
+    locked_axis: LockedAxes,
+    friction: Friction,
+}
 
-        PlayerBundle {
-            sprite_bundle: SpriteSheetBundle {
-                texture_atlas: texture_atlases.add(TextureAtlas::from_grid_with_padding(
-                    tileset.clone(),
-                    Vec2::new(tile.w as f32, tile.h as f32),
-                    tileset_definition.c_wid as usize,
-                    tileset_definition.c_hei as usize,
-                    Vec2::splat(tileset_definition.spacing as f32),
-                )),
-                sprite: TextureAtlasSprite {
-                    index: (tile.y / (tile.h + tileset_definition.spacing)) as usize
-                        * tileset_definition.c_wid as usize
-                        + (tile.x / (tile.w + tileset_definition.spacing)) as usize,
-                    ..Default::default()
-                },
-                ..Default::default()
-            },
-            input_manager: InputManagerBundle::<PlayerAction> {
-                action_state: ActionState::default(),
-                input_map: InputMap::new([
-                    (KeyCode::W, PlayerAction::Up),
-                    (KeyCode::S, PlayerAction::Down),
-                    (KeyCode::A, PlayerAction::Left),
-                    (KeyCode::D, PlayerAction::Right),
-                    (KeyCode::Up, PlayerAction::Up),
-                    (KeyCode::Down, PlayerAction::Down),
-                    (KeyCode::Left, PlayerAction::Left),
-                    (KeyCode::Right, PlayerAction::Right),
-                ]),
-            },
-            collider: Collider::cuboid(tile.w as f32, tile.h as f32),
-            ..default()
+impl From<EntityInstance> for PlayerCollision {
+    fn from(entity_instance: EntityInstance) -> Self {
+        let tile = entity_instance
+            .tile
+            .expect("Player entity must have a tile");
+
+        Self {
+            collider: Collider::cuboid((tile.w as f32 / 4.0) - 0.1, (tile.h as f32 / 4.0) - 0.1),
+            rigid_body: RigidBody::Dynamic,
+            velocity: Velocity::zero(),
+            gravity_scale: GravityScale(0.0),
+            locked_axis: LockedAxes::ROTATION_LOCKED,
+            friction: Friction::coefficient(0.0),
         }
     }
 }
@@ -91,85 +62,46 @@ enum PlayerAction {
     Right,
 }
 
-fn player_movement_system(
-    mut query: Query<(&mut Transform, &ActionState<PlayerAction>), With<Player>>,
-    collision_layer: Query<&PlayerColliders>,
-    time: Res<Time>,
-) {
-    for (mut transfrom, action_state) in query.iter_mut() {
-        let mut next_pos = transfrom.translation;
-        if action_state.pressed(PlayerAction::Up) {
-            next_pos.y += 100.0 * time.delta_seconds();
-        }
-        if action_state.pressed(PlayerAction::Down) {
-            next_pos.y -= 100.0 * time.delta_seconds();
-        }
-        if action_state.pressed(PlayerAction::Right) {
-            next_pos.x += 100.0 * time.delta_seconds();
-        }
-        if action_state.pressed(PlayerAction::Left) {
-            next_pos.x -= 100.0 * time.delta_seconds();
-        }
-        for collision_layer in collision_layer.iter() {
-            if next_pos == transfrom.translation {
-                return;
-            }
+#[derive(Bundle)]
+pub struct PlayerInput {
+    #[bundle]
+    input_manager: InputManagerBundle<PlayerAction>,
+}
 
-            if !collision_layer.positions.iter().any(|position| {
-                collide(
-                    next_pos,
-                    Vec2::new(16.0, 16.0),
-                    *position,
-                    collision_layer.item_size,
-                )
-                .is_some()
-            }) {
-                transfrom.translation = next_pos
-            }
+impl Default for PlayerInput {
+    fn default() -> Self {
+        use PlayerAction::*;
+
+        Self {
+            input_manager: InputManagerBundle::<PlayerAction> {
+                input_map: InputMap::new([
+                    (KeyCode::W, Up),
+                    (KeyCode::S, Down),
+                    (KeyCode::A, Left),
+                    (KeyCode::D, Right),
+                ]),
+                ..default()
+            },
         }
     }
 }
 
-#[derive(Component, Default)]
-struct PlayerColliders {
-    positions: Vec<Vec3>,
-    item_size: Vec2,
-}
-
-#[derive(Bundle, Default)]
-struct CollisionLayer {
-    colliders: PlayerColliders,
-}
-
-impl LdtkIntCell for CollisionLayer {
-    fn bundle_int_cell(_: IntGridCell, layer_instance: &LayerInstance) -> CollisionLayer {
-        CollisionLayer {
-            colliders: PlayerColliders {
-                positions: layer_instance
-                    .int_grid_csv
-                    .iter()
-                    .enumerate()
-                    .filter_map(|(idx, val)| {
-                        if val == &1 {
-                            Some(Vec3::new(
-                                ((idx as i32 % layer_instance.c_wid) * layer_instance.grid_size)
-                                    as f32
-                                    + 4.0,
-                                ((layer_instance.grid_size * layer_instance.c_hei)
-                                    - ((idx as i32 / layer_instance.c_wid)
-                                        * layer_instance.grid_size))
-                                    as f32
-                                    - 4.0,
-                                3.0,
-                            ))
-                        } else {
-                            None
-                        }
-                    })
-                    .collect(),
-                item_size: Vec2::splat(layer_instance.grid_size as f32),
-            },
-            ..default()
+fn player_movement_system(
+    mut query: Query<(&mut Velocity, &ActionState<PlayerAction>), With<Player>>,
+) {
+    for (mut vel, action_state) in query.iter_mut() {
+        vel.linvel = Vec2::new(0.0, 0.0);
+        if action_state.pressed(PlayerAction::Up) {
+            vel.linvel.y += 100.0;
+        }
+        if action_state.pressed(PlayerAction::Down) {
+            vel.linvel.y -= 100.0;
+        }
+        if action_state.pressed(PlayerAction::Right) {
+            vel.linvel.x += 100.0;
+        }
+        if action_state.pressed(PlayerAction::Left) {
+            vel.linvel.x -= 100.0;
         }
     }
 }
